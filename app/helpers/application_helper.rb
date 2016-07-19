b7e13bad3e3e9b71da86deca8598002449d39333
Nimrod Bahar <nimrodbahar@gmail.com>
2016-07-19 09:43:58 +0200

    Initial commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,6 @@
+module ApplicationHelper
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.jpg?s=150"
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+
+  devise_for :users
+
+end
